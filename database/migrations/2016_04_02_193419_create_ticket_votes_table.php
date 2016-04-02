2c919e033dad54829881c6fd830621c466317ddc
darkwebside <darkwebside@gmail.com>
2016-04-02 21:46:07 +0200

    Ticket and TicketVote TicketComment new models and migrations

---
+++ database/migrations/2016_04_02_193419_create_ticket_votes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketVotesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('ticket_votes', function(Blueprint $table)
+		{
+			$table->increments('id');
+
+			// Referencia al usuario que emite el voto
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+			// Referencia al Ticket por el que vota el suario
+
+			$table->integer('ticket_id')->unsigned();
+			$table->foreign('ticket_id')->references('id')->on('tickets');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('ticket_votes');
+	}
+
+}
---
+++ database/migrations/2016_04_02_194121_create_ticket_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketCommentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('ticket_comments', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('comment',255);
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('ticket_comments');
+	}
+
+}
